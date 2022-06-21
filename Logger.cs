cdd09ba79980f8b7c5f9eaae27e3bc6add5238ea
Javlon Fattoev <javlonbek.fattoev@gmail.com>
2022-06-21 12:14:46 +0500

    first commit

---
+++ Logger.cs
@@ -0,0 +1,10 @@
+﻿namespace JFA.Utils.Jwt
+{
+    public static class Logger
+    {
+        public static void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
